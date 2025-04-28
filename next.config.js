924cbf3185f54b47b0a0dc8229acfe1ea7af2bcb
Ahmed <ahmedsayedhamed38@gmail.com>
2025-04-28 22:21:21 +0300

    Sadfasd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["red-armadillo-871677.hostingersite.com"],
+    domains: [
+      "red-armadillo-871677.hostingersite.com",
+      "images.unsplash.com",
+      "source.unsplash.com",
+      "picsum.photos",
+      "i.imgur.com",
+    ],
   },
 };
 
